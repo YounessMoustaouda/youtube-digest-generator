76da1257e66b35595404f269d58bea7a3beef2a0
Youness Moustaouda <94990241+YounessMoustaouda@users.noreply.github.com>
2023-08-20 16:00:01 +0200

    Update digest_gen.py
    Added token lenght preview

--- digest_gen.py
+++ digest_gen.py
@@ -2,14 +2,16 @@ import argparse
 import os
 import openai
 from youtube_transcript_api import YouTubeTranscriptApi
+import tiktoken
 
+enc = tiktoken.get_encoding("cl100k_base") # this is the tokenizer used by gpt-3.5-turbo
 openai.api_key = "your_key"
 
 def get_subtitles(youtube_url):
     try:
         # Get video id from url
         video_id = youtube_url.split("v=")[1]
-        captions = YouTubeTranscriptApi.get_transcript(video_id)
+        captions = YouTubeTranscriptApi.get_transcript(video_id, languages=['en', 'it'])
         subtitle_text = "\n".join([caption['text'] for caption in captions])
         return subtitle_text
     except Exception as e:
@@ -20,9 +22,9 @@ def create_digest(subtitles):
     prompt = f"Summarize the following subtitles:\n{subtitles}"
     response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
-        messages=[{"role": "system", "content": "You are a helpful assistant that summarizes subtitles."},
+        messages=[{"role": "system", "content": "You are a helpful assistant that summarizes subtitles. Reply in italian if subtitles are in italian."},
                   {"role": "user", "content": prompt}],
-        max_tokens=150
+        max_tokens=600
     )
     digest = response.choices[0].message["content"].strip()
     return digest
@@ -35,6 +37,9 @@ def main():
 
     youtube_url = args.youtube_url
     subtitles = get_subtitles(youtube_url)
+    print(subtitles)
+    print(f"You are about to send an API request with {len(enc.encode(subtitles))} tokens, press enter to proceed")
+    input()
 
 
     if subtitles:
@@ -43,11 +48,11 @@ def main():
 
         # Save digest to a text file
         output_filename = "digest.txt"
-        with open(output_filename, "w") as file:
-            file.write(digest)
+        with open(output_filename, "a") as file:
+            file.write(digest+'\n')
         print(f"Digest saved to {output_filename}")
     else:
         print("No subtitles available for the provided YouTube video.")
 
 if __name__ == "__main__":
-    main()
+    main()
